3cdddf2f0bed2116e695b36b2875966ac36103ba
Bleyle <Bennybleylle09@gmail.com>
2025-08-14 23:52:42 +0300

    Delete src directory

--- src/lib/constants.ts
+++
@@ -1,7 +0,0 @@
-export function getBaseUrl() {
-  if (typeof window !== "undefined") return window.location.origin;
-  const PRODUCTION_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL;
-  return typeof PRODUCTION_URL === "string"
-    ? `https://${PRODUCTION_URL}`
-    : `http://localhost:${process.env.PORT ?? 3000}`;
-}
